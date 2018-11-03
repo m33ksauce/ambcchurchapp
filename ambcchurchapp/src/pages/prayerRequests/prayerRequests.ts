b44ee211d13a93a3c5df38dd9ad3de2c397e6816
justingatlin92 <44716985+justingatlin92@users.noreply.github.com>
2018-11-02 21:47:09 -0500

    Added Prayer Requests

---
+++ ambcchurchapp/src/pages/prayerRequests/prayerRequests.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-prayerRequests',
+  templateUrl: 'prayerRequests.html'
+})
+export class prayerRequestsPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
