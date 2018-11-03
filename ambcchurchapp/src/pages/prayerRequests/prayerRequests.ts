1128c4790a01f55ef743a090c61beeba5078900f
Chris Meek <m33ksauce@gmail.com>
2018-11-02 22:33:32 -0500

    Create a list of prayer requests for display

--- ambcchurchapp/src/pages/prayerRequests/prayerRequests.ts
+++ ambcchurchapp/src/pages/prayerRequests/prayerRequests.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { PrayerRequest } from '../../models/prayerrequest'
 
 @Component({
   selector: 'page-prayerRequests',
   templateUrl: 'prayerRequests.html'
 })
 export class prayerRequestsPage {
+  myPrayerList: Array<PrayerRequest>;
 
   constructor(public navCtrl: NavController) {
+    this.myPrayerList = new Array<PrayerRequest>();
+    var prayer = new PrayerRequest("1",
+      "Please pray for my sister",
+      "She's just really not doing well",
+      "Chris");
 
+    this.myPrayerList.push(prayer);
   }
 
 }
