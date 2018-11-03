e066dc929032c10d45c2a0b7464c62ee6c5b37a7
Chris Meek <m33ksauce@gmail.com>
2018-11-02 21:18:44 -0500

    Create prayerrequest model

---
+++ ambcchurchapp/src/models/prayerrequest.ts
@@ -0,0 +1,21 @@
+export class PrayerRequest {
+    private _id: string;
+    private _title: string;
+    private _text: string;
+    private _user: string;
+
+    constructor(id: string, title: string, text: string, user: string) {
+        this._id = id;
+        this._title = title;
+        this._text = text;
+        this._user = user;
+    }
+
+    Title(): string {
+        return this._title;
+    }
+
+    Text(): string {
+        return this._text;
+    }
+}
